e4bee4503d21da4f4526e3521cb915bb7c3a6919
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-05-14 09:50:20 -0300

    corregi el evento onclick por onsubmit

--- js/app.js
+++ js/app.js
@@ -3,18 +3,18 @@ let numeroMagico=0;
 function empezarJuego(){
     console.log("Desde funcion empezar juego")
     const startJuego=document.getElementById("comenzarJuego")
+    const numeroMagico=Math.floor(Math.random()*100)+1
+    console.log(numeroMagico)
     if(startJuego.textContent=="Comenzar el juego"){
         startJuego.classList.add("d-none")
     }else{
         startJuego.remove("d-none")
     }
-    numeroMagico=Math.floor(Math.random()*100)+1
-    console.log(numeroMagico)
 }
 
-function chequearNumeroIngresado(){
+function chequearNumeroIngresado(e){
     console.log("Desde funcion chequear numero ingresado")
-    let numeroIngresado=document.getElementById("numero")
+    let numeroIngresado=document.getElementById("numero").value
     if(numeroIngresado===numeroMagico){
         alert("GANASTEEE! 🍾🍻🙌🎊😎")
     } else if(numeroIngresado<numeroMagico) {
